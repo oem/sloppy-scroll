af70b42ac88ab3bfada37da9d6ad1fe22692d502
Ömür Özkir <oemuer.oezkir@gmail.com>
2017-02-04 13:19:51 +0100

    webpack will automatically compress now

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
-// webpack configuration
+var webpack = require('webpack');
+
 module.exports = {
   entry: './src/main.js',
   output: {
-    filename: './dist/sloppy-scroll.js'
-  }
+    path: './dist',
+    filename: 'sloppy-scroll.min.js'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  ]
 };
